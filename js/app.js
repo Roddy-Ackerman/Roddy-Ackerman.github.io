1bb57bf0b0c4920b0764acfc39adebc67352dd7c
Roddy Ackerman <64042864+Roddy-Ackerman@users.noreply.github.com>
2021-01-29 16:03:51 -0500

    Delete app.js

--- js/app.js
+++
@@ -1,5 +0,0 @@
-https://api.instagram.com/oauth/authorize
-  ?client_id={1528528410672199}
-  &redirect_uri={https://roddy-ackerman.github.io/}
-  &scope=user_profile,user_media
-  &response_type=code
